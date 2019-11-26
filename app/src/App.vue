555101bc802aad7ee9189416205f908802ac9718
zengjing73 <17780835313@163.com>
2019-11-26 19:07:02 +0800

    添加了主题色

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,13 +2,12 @@
   <div id="app">
     <div id="nav">
       <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
+      <router-link to="/about">About</router-link>-->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="less">
-@import './assets/style/base.less';
-
+@import "./assets/style/base.less";
 </style>
