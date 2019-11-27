69e54aa124db491a713208dbf14f5a7ee287033f
zengjing73 <17780835313@163.com>
2019-11-27 10:32:26 +0800

    添加了resize.css

--- app/src/main.js
+++ app/src/main.js
@@ -4,6 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import 'amfe-flexible'
+import './assets/style/resize.css'
+
 
 Vue.config.productionTip = false
 
