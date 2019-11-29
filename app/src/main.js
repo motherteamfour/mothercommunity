f507f4df0ed73eddb7dc50bfe05cdca17056d078
zengjing73 <17780835313@163.com>
2019-11-29 15:53:23 +0800

    对axios进行了二次封装

--- app/src/main.js
+++ app/src/main.js
@@ -8,10 +8,9 @@ import './assets/style/resize.css'
 import './assets/font-awesome/css/font-awesome.min.css'
 
 
-// 引入请求库
-import axios from 'axios'
+import http from './utils/http'
 import vueAxios from 'vue-axios'
-Vue.use(vueAxios, axios)
+Vue.use(vueAxios, http)
 
 Vue.config.productionTip = false
 
